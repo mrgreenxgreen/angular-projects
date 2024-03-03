dad7d428626b2852af7a83b4e9153a81664d3b1f
unknown <davedalimocon@gmail.com>
2024-03-04 04:59:41 +0800

    routing

--- random-one/src/app/app.routes.ts
+++ random-one/src/app/app.routes.ts
@@ -1,13 +1,40 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './main/home/home.component';
 import { GalleryComponent } from './main/gallery/gallery.component';
+import { MainComponent } from './main/main.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
 export const routes: Routes = [
+    { path: '', redirectTo: '/main', pathMatch: 'full' },
     {   
-        path:"home",
-        component:HomeComponent
+        path:"main",
+        component:MainComponent,
+        children:[
+                {
+                    path:"home",
+                    component:HomeComponent
+                },
+                {
+                    path:"gallery",
+                    component:GalleryComponent
+                },
+        ]
     },
     {   
-        path:"gallery",
-        component:GalleryComponent
+        path:"auth",
+        component:AuthComponent,
+        children:[
+            {
+                path:"login",
+                component:LoginComponent
+            },
+            {
+                path:"register",
+                component:RegisterComponent
+            },
+
+        ]
     }
 ];
--- random-one/src/app/main/main.component.ts
+++ random-one/src/app/main/main.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
+
 export class MainComponent {
 
 }
